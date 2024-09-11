c2702423ed9572d33a122188615589f562e08755
Sergio Socha <sergio_socha@yahoo.com>
2024-09-10 20:17:10 -0500

    Agrega proyecto spring boot con packages
    Agrega proyecto spring boot generado, con packages creados para controlador, db, logica

---
+++ src/main/java/com/salud/arqui/ArquiApplication.java
@@ -0,0 +1,13 @@
+package com.salud.arqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salud/arqui/ArquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salud.arqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
