16848f02925772909492dbeefa02283cc280ac54
Cristian Soto <101820809+CristianSz2003@users.noreply.github.com>
2024-09-13 19:03:58 -0500

    Revert "Agrega feature guardarAfiliado"

--- src/main/java/com/salud/arqui/controller/AfiliadoController.java
+++ src/main/java/com/salud/arqui/controller/AfiliadoController.java
@@ -1,24 +1,4 @@
 package com.salud.arqui.controller;
 
-
-import com.salud.arqui.controller.dto.AfiliadoDTO;
-import com.salud.arqui.logica.AfiliadoService;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@AllArgsConstructor
 public class AfiliadoController {
-
-    private AfiliadoService afiliadoService;
-
-    @PostMapping(path = "/afiliado")
-    public String guardarAfiliado(@RequestBody AfiliadoDTO afiliadoDTO){
-        afiliadoService.guardarAfiliado(afiliadoDTO.nombre(), afiliadoDTO.edad(),afiliadoDTO.email(),afiliadoDTO.genero());
-        return "afiliado guardado con exito";
-    }
-
 }
