fe7ddaab36ffe0223ce52fb770d7dd89d16e2ba2
sergi <sergio_socha@yahoo.com>
2024-09-15 18:49:12 -0500

    Agrega feature actualizarAfiliado, actualizarBeneficiario

--- src/main/java/com/salud/arqui/db/jpa/BeneficiarioJPA.java
+++ src/main/java/com/salud/arqui/db/jpa/BeneficiarioJPA.java
@@ -11,4 +11,6 @@ import java.util.Optional;
 public interface BeneficiarioJPA extends JpaRepository<BeneficiarioORM, Long> {
 
     Optional<BeneficiarioORM> findByAfliliadoORM(AfiliadoORM afiliado);
+
+
 }
