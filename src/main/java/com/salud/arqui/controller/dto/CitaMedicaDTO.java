e12b8809998a80ce8bf198cdcbe4bb5bd3ed9b6e
crist <cristanchos2003@gmail.com>
2024-09-16 21:32:04 -0500

    Agregado consulta y relacion HistorialMedico

--- src/main/java/com/salud/arqui/controller/dto/CitaMedicaDTO.java
+++ src/main/java/com/salud/arqui/controller/dto/CitaMedicaDTO.java
@@ -1,6 +1,6 @@
 package com.salud.arqui.controller.dto;
 
 public record CitaMedicaDTO(String tipoDeCita, String descripcion, String fechaConsulta, Long idAfiliado,
-                            Long idBeneficiario){
+                            Long idBeneficiario, Long idHistorialMedico){
 
 }
