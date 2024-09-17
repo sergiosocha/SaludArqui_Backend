2798dd2fe874c636bdfae5239bf37d7372a89d52
crist <cristanchos2003@gmail.com>
2024-09-17 09:49:04 -0500

    Agregado relación cita medica e historial medico

--- src/main/java/com/salud/arqui/logica/CitaMedicaService.java
+++ src/main/java/com/salud/arqui/logica/CitaMedicaService.java
@@ -3,9 +3,11 @@ package com.salud.arqui.logica;
 import com.salud.arqui.db.jpa.AfiliadoJPA;
 import com.salud.arqui.db.jpa.BeneficiarioJPA;
 import com.salud.arqui.db.jpa.CitaMedicaJPA;
+import com.salud.arqui.db.jpa.HistorialMedicoJPA;
 import com.salud.arqui.db.orm.AfiliadoORM;
 import com.salud.arqui.db.orm.BeneficiarioORM;
 import com.salud.arqui.db.orm.CitaMedicaORM;
+import com.salud.arqui.db.orm.HistorialMedicoORM;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,7 @@ public class CitaMedicaService {
     private final AfiliadoJPA afiliadoJPA;
     private final BeneficiarioJPA beneficiarioJPA;
     private final HistorialMedicoService historialMedicoService;
+    private final HistorialMedicoJPA historialMedicoJPA;
 
     public boolean guardarCitaMedica(String tipoDeCita, String descripcion, LocalDate fechaConsulta, Long idAfiliado) {
 
@@ -38,22 +41,22 @@ public class CitaMedicaService {
         BeneficiarioORM beneficiario = beneficiarioJPA.findByAfliliadoORM(afiliado).orElseThrow(()->
                 new IllegalArgumentException("No hay beneficiario asociado al afiliado"+ afiliado));
 
+        HistorialMedicoORM historialMedico = historialMedicoJPA.findById(idAfiliado).orElseThrow(()->
+                new IllegalArgumentException("El ID del afiliado no existe."));
+
         CitaMedicaORM nuevaCitaMedica = new CitaMedicaORM();
         nuevaCitaMedica.setDescripcion(descripcion);
         nuevaCitaMedica.setFecha_consulta(fechaConsulta);
         nuevaCitaMedica.setTipo_de_cita(tipoDeCita);
         nuevaCitaMedica.setAfiliadoORM(afiliado);
         nuevaCitaMedica.setBeneficiarioORM(beneficiario);
-
+        nuevaCitaMedica.setHistorialMedicoORM(historialMedico);
 
 
         citaMedicaJPA.save(nuevaCitaMedica);
 
         historialMedicoService.guardarHistorialMedica(tipoDeCita, descripcion);
         return true;
-
-
-
 
     }
 
