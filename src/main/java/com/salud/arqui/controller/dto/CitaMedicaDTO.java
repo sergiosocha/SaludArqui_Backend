ddef8ffb7c8846d93fea6c6918f09805ccb4e8e5
sergi <sergio_socha@yahoo.com>
2024-09-18 12:41:12 -0500

    corrige  HistorialMedicoController.java HistorialMedicoService, controller

--- src/main/java/com/salud/arqui/controller/dto/CitaMedicaDTO.java
+++ src/main/java/com/salud/arqui/controller/dto/CitaMedicaDTO.java
@@ -1,5 +1,5 @@
 package com.salud.arqui.controller.dto;
 
-public record CitaMedicaDTO(String tipoDeCita, String descripcion, String fechaConsulta, Long idAfiliado,Long idBeneficiario, Long idHistorialMedico){
+public record CitaMedicaDTO(String tipoDeCita, String descripcion, String fechaConsulta, Long idAfiliado,Long idBeneficiario){
 
 }
