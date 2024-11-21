15889252ae003191d623a0b0e5609c8e9a285ecd
sergi <sergio_socha@yahoo.com>
2024-11-21 17:28:43 -0500

    Delete unused imports

--- src/test/java/com/salud/arqui/logica/BeneficiarioServiceTest.java
+++ src/test/java/com/salud/arqui/logica/BeneficiarioServiceTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 import java.util.Optional;
